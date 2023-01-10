170b12fc39f62e372516ec5d2d1c5402884b6659
Rusakow <yegorsr0@gmail.com>
2023-01-11 01:51:33 +0300

    Deduplicatior done
    Renamed a file
    Deduplicator released

--- Deduplicator.py
+++ Deduplicator.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import List, Tuple
 import os
 import sqlite3 as sql
+from math import ceil
 
 
 class Compressor:
@@ -36,7 +37,7 @@ class Compressor:
 
                 yield chunk
 
-    def create_table(self, file_db: str):
+    def create_database(self, file_db: str):
         CREATE_TABLE_STATEMENT = """
             CREATE TABLE IF NOT EXISTS chunk_table(
                 id INTEGER PRIMARY KEY,
@@ -48,12 +49,19 @@ class Compressor:
         """
 
         conn = sql.connect(file_db)
-        cur = conn.cursor()
-        cur.execute(CREATE_TABLE_STATEMENT)
+        conn.execute(CREATE_TABLE_STATEMENT)
+        conn.execute("""PRAGMA synchronous = OFF;""")
+        # conn.execute("""PRAGMA journal_mode = WAL;""")
         conn.commit()
         print(f"SQL DB created at {file_db}")
 
         return conn
+
+    def save_byte_size(self, byte_size: int, conn: sql.Connection):
+        conn.execute("""CREATE TABLE IF NOT EXISTS metainfo(byte_size INTEGER);""")
+        conn.commit()
+        conn.execute("""INSERT INTO metainfo (byte_size) VALUES (?)""", (byte_size,))
+        conn.commit()
 
     def put_to_db(self, byte_chunk: str, conn: sql.Connection) -> str:
         """
@@ -62,48 +70,46 @@ class Compressor:
         :return: hash str
         """
 
-        cur = conn.cursor()
         chunk_hash = self.HASH_FUNC(byte_chunk)
 
-        row = cur.execute("""SELECT * FROM chunk_table WHERE hash = ?;""", (chunk_hash,)).fetchone()
+        row = conn.execute("""SELECT * FROM chunk_table WHERE hash = ?;""", (chunk_hash,)).fetchone()
 
         if row is None:
-            cur.execute("""INSERT INTO chunk_table (hash, data, reuse_cnt) VALUES (?, ?, ?);""", (chunk_hash, byte_chunk, 1))
+            conn.execute("""INSERT INTO chunk_table (hash, data, reuse_cnt) VALUES (?, ?, ?);""", (chunk_hash, byte_chunk, 1))
             conn.commit()
-            row = cur.execute("""SELECT * FROM chunk_table WHERE hash = ?;""", (chunk_hash,)).fetchone()
+            row = conn.execute("""SELECT * FROM chunk_table WHERE hash = ?;""", (chunk_hash,)).fetchone()
             return row[0]
         else:
             row_id = row[0]
             reuse_cnt = row[3]
-            cur.execute("""UPDATE chunk_table SET reuse_cnt = ? WHERE id = ?;""", (reuse_cnt + 1, row_id))
+            conn.execute("""UPDATE chunk_table SET reuse_cnt = ? WHERE id = ?;""", (reuse_cnt + 1, row_id))
             conn.commit()
 
             return row_id;
 
-
-    def compress(self, file_to_compress) -> List[str]:
+    def compress(self, file_to_compress) -> Tuple[str, str]:
         db_file = os.path.splitext(file_to_compress)[0] + ".db"
         compressed_file = os.path.splitext(file_to_compress)[0] + ".bin"
 
-        conn = self.create_table(db_file)
+        conn = self.create_database(db_file)
+        id_arr = []
 
         for byte_chunk in self.split_to_chunks(file_to_compress):
             id = self.put_to_db(byte_chunk, conn)
-            print(f"Chunk: {byte_chunk}  -- {id}, {type(id)}")
+            id_arr.append(id)
 
+        max_byte_len = ceil((len(bin(max(id_arr))) - 2) / 8)
+        self.save_byte_size(max_byte_len, conn)
 
+        with open(compressed_file, "wb") as f:
+            f.write(b"".join(i.to_bytes(length=max_byte_len, byteorder="big", signed=False) for i in id_arr))
 
-
-        hash_chain = []
-        chunk_data_dict = {}
-
-
-
+        return compressed_file, db_file
 
 
 if __name__ == "__main__":
     c = Compressor()
-    file = "/home/jegor/PycharmProjects/sabd_data_deduplication/test_files/test.txt"
+    file = "/home/jegor/PycharmProjects/sabd_data_deduplication/test_files/photo.jpg"
     c.compress(file)
 
 
